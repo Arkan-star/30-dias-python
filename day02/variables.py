526637ab3a2933a08705075adc6f7dd6fdf1f358
Arkan Lex <ocampopablo994@gmail.com>
2025-08-20 15:33:16 -0300

    Day 02 - Variables

---
+++ day02/variables.py
@@ -0,0 +1,23 @@
+#Variables
+nombre = "Arkan" #String (texto)
+edad =  19 #Int (numeros)
+altura = 1.67 #Float (numeros con punto o decimales)
+estudiante = False #Booleano (Verdadero o falso)
+
+print(type(nombre)) #Muestro en pantalla con print y con type hace que solo muestre el tipo de variable
+print(type (edad))
+print(type (altura))
+print(type (estudiante))
+
+#Operadores
+
+a = 5 #Creo una variable numeral normal
+b = 7
+c = a + b #Operador de suma
+d = b - a #
+e = a * b
+f = a / b
+g = a % b
+
+print (a + b) #Sumo a + b, seria 12
+print (a + b - 5) #Lo mismo que antes, pero le agrego una resta con numero = 7
